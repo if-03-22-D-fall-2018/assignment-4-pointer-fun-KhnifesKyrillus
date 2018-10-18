ca03fc7864bcbd4a418b3e6de4b99d91a3ed3091
Kyrillus Khnifes <khnifes.kyrillus@gmail.com>
2018-10-18 09:23:02 +0200

    final version

--- PointerFun.c
+++ PointerFun.c
@@ -1,13 +1,14 @@
 #include "stdio.h"
 
 void print_integers(int int_value, int* int_pointer);
-void change_integers(int* int_value, int** int_pointer);
+void change_integers(int int_value, int* int_pointer);
+
 int main(int argc, char const *argv[])
 {
   int int_value=7;
-  int* int_pointer= &int_value;//
+  int* int_pointer= &int_value;//Man könnte rein theoretisch irgendeine Adresse nehemen, jedoch ist dabei die Gefahr groß das man eine schon resevierte Adresse nimmt(z.B. Betriebssystem), dies vermeiden wir indem wir die Adresse von int_value nehmen.
   print_integers(int_value,int_pointer);
-  change_integers(&int_value,&int_pointer);
+  change_integers(int_value,int_pointer);
   print_integers(int_value,int_pointer);
   //
   return 0;
@@ -18,8 +19,8 @@ void print_integers(int int_value, int* int_pointer)
   printf("Got an integer value %d and an address to an integer with value %d\n\n",int_value, *int_pointer );
 }
 
-void change_integers(int* int_value, int** int_pointer)
+void change_integers(int int_value, int* int_pointer)
 {
-  *int_value=361;
-  int_pointer= &int_value;
+  int_value=361;
+  *int_pointer= int_value;
 }
