8f40384e1385df442b041ff78603e06cf7670209
Kyrillus Khnifes <khnifes.kyrillus@gmail.com>
2018-10-18 08:32:32 +0200

    created print_integers, it works

--- PointerFun.c
+++ PointerFun.c
@@ -1,8 +1,16 @@
 #include "stdio.h"
 
+void print_integers(int int_value, int* int_pointer);
+
 int main(int argc, char const *argv[])
 {
-  int int_value;
-  int int_pointer;
+  int int_value=7;
+  int* int_pointer= &int_value;
+  print_integers(int_value,int_pointer);
   return 0;
+}
+
+void print_integers(int int_value, int* int_pointer)
+{
+  printf("Got an integer value %d and an address to an integer with value %d\n",int_value, *int_pointer );
 }
